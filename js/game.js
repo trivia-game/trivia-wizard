edaa29fc163b80b0137f6ba28ee938dc10c34de8
Carl Olson <colson10@gmail.com>
2018-01-29 16:43:47 -0800

    added random number and shuffle functions and beginning of main question function

--- js/game.js
+++ js/game.js
@@ -2,6 +2,9 @@
 
 Question.allQuestions = [];
 
+var sectionEl = document.getElementById('questions');
+var formEl = document.getElementById('answers');
+
 function Question(question, answer, setOfAnswers) {
   this.question = question;
   this.answer = answer;
@@ -16,3 +19,53 @@ new Question('Bruce Banner turns into what fictional superhero when he becomes a
 new Question('Which actor played Marty McFly in the 1980\'s sci-fi classic Back to the Future?', 'Michael J. Fox', ['Michael J. Fox', 'John Cusak', 'Tom Cruise', 'Michael Keaton']);
 new Question('Tyler Durden is a fictional character appearing as the central protagonist and antagonist in what 1999 american film?', 'Fight Club', ['Fight Club', 'The Matrix', 'The Green Mile', 'The Sixth Sense']);
 
+function randomNumGenerator(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function shuffle(array) {
+  var currentIndex = array.length;
+  var temporaryValue; 
+  var randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function gameQuestions() {
+  var rand = randomNumGenerator(0, Question.allQuestions.length);
+  var q1 = Question.allQuestions[rand];
+  var pEl = document.createElement('p');
+  pEl.textContent = q1.question;
+  sectionEl.appendChild(pEl);
+  var answerArray = shuffle(q1.setOfAnswers);
+  for (var i in answerArray) {
+    var letterIndex;
+    if (i === 0) {
+      letterIndex = 'A';
+    } else if (i === 1) {
+      letterIndex = 'B';
+    } else if (i === 2) {
+      letterIndex = 'C';
+    } else {
+      letterIndex = 'D';
+    }
+    var button = document.createElement('button');
+    button.innerHTML = '<span>' + letterIndex + '</span>' + answerArray[i];
+    formEl.appendChild(button);
+  }
+}
+
+gameQuestions();
